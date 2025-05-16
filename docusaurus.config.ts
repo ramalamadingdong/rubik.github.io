f0e66bc60945aae9dd13dc095f10441a10a2d7d9
Rami Mouro <ramimouro@gmail.com>
2025-05-15 22:30:02 -0600

    fixed broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/Intro/intro',
             },
           ],
         },
