8e4ea23bc65b5567cf6eb4fbe8d43e395fce25f6
Rami Mouro <ramimouro@gmail.com>
2025-07-04 18:04:42 -0600

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://first-rubik.github.io',
+  url: 'https://ramalamadingdong.github.io/rubik.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rubik.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
